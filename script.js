e9e6d06aaae1a94a9b6ec3363aa5ee6a82e43d29
shuvajit gayen <shuvajitgayen57@gmail.com>
2024-11-22 21:06:40 +0530

    morden portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+const tablinks = document.getElementsByClassName("tab-links")
+const tabcontents = document.getElementsByClassName("tab-contents")
+
+function opentab(tabname){
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-links")
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-links")
+    document.getElementById(tabname).classList.add("active-tab")
+}
