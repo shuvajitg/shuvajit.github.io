96d0a1c0a16ce98aa9d1781b6d35fd5f783e64ea
shuvajit gayen <shuvajitgayen57@gmail.com>
2024-11-24 19:32:49 +0530

    optimize

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function opentab(tabname) {
 
 const sideMenu = document.getElementById("sidebar");
 const submit = document.getElementById("submit");
+submit.innerHTML= "Send"
+
 
 function openmenu() {
   sideMenu.style.right = "0px";
@@ -25,16 +27,18 @@ function closemenu() {
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbyVD5jqYqKTZmWUrhr8mSs-QEyYt-m84ON7bCbLG2pPAVghOtw15BHa6Fl8iY-iK8gY/exec";
 const form = document.forms["submit-to-google-sheet"];
-
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", (e) => {
+  submit.innerHTML= "Loading.."
+    setTimeout(()=>{
+    submit.innerHTML= "Send"
+    },4000)
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
 //   submit.innerHTML= "Loading.."
     .then((response) => {
         msg.innerHTML = "Message Sended successfully !";
-
         setTimeout(() => {
             msg.innerHTML = "";  // Clear the message after 5 seconds.
         },5000)
